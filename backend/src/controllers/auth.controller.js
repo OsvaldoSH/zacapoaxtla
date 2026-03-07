9583f4cf82a0826096deeec1a0cf096169c9526d
OsvaldoSH <osvaldo.sal.h@gmail.com>
2026-03-07 00:42:51 -0600

    Trabajando en el backend

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,37 @@
+import pool from "../config/db.js";
+
+export const login = async (req, res) => {
+    const { usuario, contrasena } = req.body;
+
+    if (!usuario || !contrasena) {
+        return res.status(400).json({ error: "Faltan datos"});
+    }
+
+    try {
+        const rows = await pool.query(
+            "select * from usuarios where usuario = ?",
+            [usuario]
+        );
+
+        if (rows.length === 0 || rows[0].contrasena !== contrasena) {
+            return res.status(401).json({ error: "Usuario o contrasena incorrecta"});
+        }
+
+        if (rows[0].estado !== "activo") {
+            return res.status(401).json({ error: "Usuario no activo"});
+        }
+
+        res.json({
+            mensaje: "Login correcto",
+            usuario: {
+                id: rows[0].id,
+                nombre: rows[0].nombre,
+                usuario: rows[0].usuario,
+                rol: rows[0].rol,
+            }
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Error del servidor" });
+    }
+};
---
+++ backend/src/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import pool from "./config/db.js"
+import authRoutes from "./routes/auth.routes.js"
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+
+
+app.get("/", (req, res) => {
+    res.send("Servidor funcionando");
+});
+
+pool.getConnection()
+    .then(conn => {
+        console.log("Conectado a MariaDB");
+        conn.release();
+    })
+    .catch(err => {
+        console.error("Error de conexion:", err);
+    })
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en puerto ${PORT}`);
+})
+
