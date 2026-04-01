938d30a374eefd8285ef68e023593dfbc8039162
OsvaldoSH <osvaldo.sal.h@gmail.com>
2026-04-01 01:15:53 -0600

    se agrego gitignore

--- backend/src/routes/usuarios.routes.js
+++ backend/src/routes/usuarios.routes.js
@@ -1,8 +1,11 @@
 import express from "express";
-import { getUsuarios } from "../controllers/usuarios.controller.js";
+import { crearUsuario, getUsuarios } from "../controllers/usuarios.controller.js";
 import { verificarToken } from "../middlewares/auth.middlewares.js";
 
 const router = express.Router();
+
 router.get("/", verificarToken, getUsuarios);
+
+router.post("/", verificarToken, crearUsuario);
 
 export default router;
